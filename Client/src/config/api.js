2fb2552921341141f2c8cfa6506bc93a2eada5b7
Aman-Charde <amancharde8989@gmail.com>
2025-12-10 13:00:26 +0530

    apijs

--- Client/src/config/api.js
+++ Client/src/config/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Get API URL from environment or use default
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
+const API_URL = process.env.REACT_APP_API_URL || "https://workalpha.onrender.com";
 
 // Create axios instance with default config
 const api = axios.create({
